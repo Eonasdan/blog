8e0d806f9afa9bac2157a7d7a4c3fcb7696e2a7a
Eonasdan <me@eonasdan.com>
2020-10-06 19:45:33 -0400

    make creates full html files hopefully to fix meta tags

--- js/post.js
+++ js/post.js
@@ -6,74 +6,12 @@ function noPost() {
     document.getElementById('nextPager').style.display = 'none';
 }
 
-function onLinkClick(e) {
+/*function onLinkClick(e) {
     e.preventDefault();
     const file = e.target.dataset.file;
     getPost(file);
     window.history.pushState(null, null, file);
-}
-
-function getPost(file) {
-    fetch(`./posts/${file}.html`)
-        .then(response => {
-            if (!response.ok) {
-                noPost();
-                throw new Error('No post found');
-            }
-            return response.text();
-        })
-        .then(function (html) {
-            const doc = new DOMParser().parseFromString(html, "text/html");
-            document.getElementById('post-inner').innerHTML = doc.querySelector('article').innerHTML;
-            //document.head.innerHTML += doc.querySelector('post-head').innerHTML;
-
-            const post = posts.find(x => x.file === file);
-            let postDate = new Date();
-            if (post) {
-                document.title = post.title;
-                if (post.thumbnail) {
-                    document.getElementById('post-thumbnail').innerHTML = `<img src="/img/${post.thumbnail}" alt="" class="img-fluid"/>`;
-                    document.getElementById('metaImage').setAttribute("content", `//${location.host}/img/${post.thumbnail}`);
-                } else {
-                    document.getElementById('post-thumbnail').innerHTML = '';
-                }
-                if (post.postDate) postDate = new Date(post.postDate);
-                /*const shareWidget = document.getElementsByClassName('share-post-widget')[0];
-                shareWidget.getElementsByClassName('facebook')[0].href = `https://www.facebook.com/sharer/sharer.php?u=${location.href}`;
-                shareWidget.getElementsByClassName('twitter')[0].href = `https://twitter.com/intent/tweet?text=CHeck out this blog post from @Eonasdan. ${location.href}`;
-                shareWidget.getElementsByClassName('linkedin')[0].href = `https://www.linkedin.com/sharing/share-offsite/?url=${location.href}`;*/
-
-                document.getElementById('metaTitle').setAttribute("content", post.title);
-                document.getElementById('metaDescription').setAttribute("content", post.excerpt);
-                document.getElementById('metaUrl').setAttribute("content", location.href);
-            }
-
-            function updatePager(selector, post) {
-                if (post.thumbnail) {
-                    selector.getElementsByClassName('single-feature-post')[0].style.backgroundImage = `url(/img/${post.thumbnail})`;
-                } else {
-                    selector.getElementsByClassName('single-feature-post')[0].style.backgroundImage = '';
-                }
-                selector.getElementsByClassName('post-title')[0].innerHTML = post.title;
-                selector.getElementsByClassName('post-title')[0].dataset.file = post.file;
-
-                /* if (post.excerpt) {
-                     selector.getElementsByClassName('post-excerpt')[0].innerHTML = post.excerpt;
-                 }*/
-                selector.style.display = 'block'
-            }
-
-            const index = posts.findIndex(x => x === post);
-            const pastPost = posts[index + 1];
-            const futurePost = posts[index - 1];
-
-            if (pastPost) updatePager(document.getElementById('previousPager'), pastPost);
-            else document.getElementById('previousPager').style.display = 'none';
-
-            if (futurePost) updatePager(document.getElementById('nextPager'), futurePost);
-            else document.getElementById('nextPager').style.display = 'none';
-        });
-}
+}*/
 
 function showPosts(filteredPosts) {
     let html = ''
@@ -88,7 +26,7 @@ function showPosts(filteredPosts) {
                             </div>
                             <div class="col-12 col-md-6">
                                 <div class="post-content mt-0">
-                                    <a href="/${post.file}" class="post-title mb-2">${post.title}</a>
+                                    <a href="/posts/${post.file}" class="post-title mb-2">${post.title}</a>
                                     <div class="post-meta d-flex align-items-center mb-2">
                                         <span class="post-author">By Jonathan Peterson</span>
                                         <i class="far fa-circle" aria-hidden="true"></i>
@@ -108,7 +46,7 @@ function search(term) {
     showPosts(posts.filter(x => x.title.includes(term) || x.body.includes(term)));
 }
 
-fetch('./posts/posts.json')
+fetch('/posts/posts.json')
     .then(response => response.json())
     .then(data => {
         posts = data;
@@ -116,13 +54,8 @@ fetch('./posts/posts.json')
         const term = urlParams.get('search');
         if (term) {
             search(term)
-        } else if (redirect) {
-            getPost(location.pathname.substring(1));
         } else if (location.pathname === '/') {
             showPosts(posts);
-        } else {
-            noPost();
         }
     });
-//vizew-pager
-[...document.querySelectorAll('.single-feature-post .post-title')].forEach(element => element.addEventListener('click', onLinkClick));
+//[...document.querySelectorAll('.single-feature-post .post-title')].forEach(element => element.addEventListener('click', onLinkClick));
--- make.js
+++ make.js
@@ -3,35 +3,99 @@ const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
 const path = require('path');
 
-const details = (file) => {
-    const html = fs.readFileSync(`./posts/${file}`, 'utf8');
-    const metaTag = new JSDOM(html).window.document.querySelector("post-meta");
-    const article = new JSDOM(html).window.document.querySelector("article");
+function stripHtml(html, replaceDoubleSpaceWith) {
+    replaceDoubleSpaceWith = replaceDoubleSpaceWith || '';
+    return html.textContent.replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g, replaceDoubleSpaceWith);
+}
+
+function removeDirectory(directory, removeSelf) {
+    if (removeSelf === undefined) removeSelf = true;
+    try {
+        const files = fs.readdirSync(directory) || [];
+        files.forEach(file => {
+            const filePath = path.join(directory, file);
+            if (fs.statSync(filePath).isFile())
+                fs.unlinkSync(filePath);
+            else
+                removeDirectory(filePath);
+        })
+    } catch (e) {
+        return;
+    }
+    if (removeSelf)
+        fs.rmdirSync(directory);
+}
+
+
+//remove old stuff
+removeDirectory('./posts', false);
+
+const posts = fs
+    .readdirSync('./post_partials')
+    .filter(file => path.extname(file).toLowerCase() === '.html')
+
+//create meta info
+let postsMeta = [];
+
+//create post files
+//read post template
+const postTemplate = fs.readFileSync(`./post-template.html`, 'utf8');
+posts.forEach(file => {
+    const newPageDocument = new JSDOM(postTemplate).window.document;
+    const html = fs.readFileSync(`./post_partials/${file}`, 'utf8');
+    const article = new JSDOM(html).window.document.querySelector('article');
+    newPageDocument.getElementById('post-inner').innerHTML = article.innerHTML;
+
+    const metaTag = new JSDOM(html).window.document.querySelector('post-meta');
+    let hasMeta = metaTag && stripHtml(metaTag) !== '';
+
     // strip html tags, line breaks and extra spaces
-    const articleBody = article.textContent.replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g, ' ');
+    const articleBody = stripHtml(article, ' ');
 
     let baseMeta = {
-        file: file.replace(path.extname(file),''),
+        file: file,
         title: file.replace(path.extname(file), ''),
         body: articleBody,
-        "postDate": fs.statSync(`./posts/${file}`).mtime
+        "postDate": fs.statSync(`./post_partials/${file}`).mtime
     };
 
-    if (!metaTag || metaTag.textContent.replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g, '') === '') return baseMeta;
+    if (!hasMeta) {
+        postsMeta.push(baseMeta);
+    } else {
+        const postMeta = JSON.parse(metaTag.textContent);
 
-    const postMeta = JSON.parse(metaTag.textContent);
-    return {
-        ...baseMeta,
-        ...postMeta
-    };
-};
+        newPageDocument.title = postMeta.title;
+        if (postMeta.thumbnail) {
+            newPageDocument.getElementById('post-thumbnail').innerHTML = `<img src="/img/${postMeta.thumbnail}" alt="" class="img-fluid"/>`;
+            newPageDocument.getElementById('metaImage').setAttribute("content", `/img/${postMeta.thumbnail}`);
+        } else {
+            newPageDocument.getElementById('post-thumbnail').innerHTML = '';
+        }
+        /*const shareWidget = document.getElementsByClassName('share-post-widget')[0];
+        shareWidget.getElementsByClassName('facebook')[0].href = `https://www.facebook.com/sharer/sharer.php?u=${location.href}`;
+        shareWidget.getElementsByClassName('twitter')[0].href = `https://twitter.com/intent/tweet?text=CHeck out this blog post from @Eonasdan. ${location.href}`;
+        shareWidget.getElementsByClassName('linkedin')[0].href = `https://www.linkedin.com/sharing/share-offsite/?url=${location.href}`;*/
 
-let posts = fs
-    .readdirSync('./posts')
-    .filter(file => path.extname(file).toLowerCase() === '.html')
-    .map(file => details(file))
+        newPageDocument.getElementById('metaTitle').setAttribute("content", postMeta.title);
+        newPageDocument.getElementById('metaDescription').setAttribute("content", postMeta.excerpt);
+        newPageDocument.getElementById('metaUrl').setAttribute("content", `https://eonasdan.com/posts/${file}`);
+
+        postsMeta.push({
+            ...baseMeta,
+            ...postMeta
+        });
+    }
+
+    fs.writeFileSync(`./posts/${file}`,
+        `<!DOCTYPE html>
+<html lang="en">${newPageDocument.getElementsByTagName('html')[0].innerHTML}
+</html>`);
+});
+
+
+postsMeta = postsMeta
     .sort((a, b) => {
         return +new Date(a.postDate) > +new Date(b.postDate) ? -1 : 0;
     });
 
-fs.writeFileSync('./posts/posts.json', JSON.stringify(posts, null, 2));
+fs.writeFileSync('./posts/posts.json', JSON.stringify(postsMeta, null, 2));
