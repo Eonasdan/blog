50a070a56ea52d191e4cd2be8f8b46f253668d1d
Eonasdan <me@eonasdan.com>
2020-11-05 08:53:17 -0500

    blog improvements

--- js/post.js
+++ js/post.js
@@ -1,12 +1,3 @@
-let posts = [];
-
-/*function onLinkClick(e) {
-    e.preventDefault();
-    const file = e.target.dataset.file;
-    getPost(file);
-    window.history.pushState(null, null, file);
-}*/
-
 function showPosts(filteredPosts) {
     let html = ''
     if (!filteredPosts || filteredPosts.length === 0) html = '<h1>No results</h1>';
@@ -35,54 +26,16 @@ function showPosts(filteredPosts) {
     document.getElementById('post-container').innerHTML = html;
 }
 
-function search(term) {
-    if (!term) return;
-    showPosts(posts.filter(x => x.title.includes(term) || x.body.includes(term)));
+const urlParams = new URLSearchParams(window.location.search);
+const term = urlParams.get('search');
+if (term) {
+    fetch('/posts/posts.json')
+        .then(response => response.json())
+        .then(data => {
+            showPosts(data.filter(x => x.title.includes(term) || x.body.includes(term)));
+        });
 }
 
-function updatePager(selector, post) {
-    if (post.thumbnail) {
-        selector.getElementsByClassName('single-feature-post')[0].style.backgroundImage = `url(/img/${post.thumbnail})`;
-    } else {
-        selector.getElementsByClassName('single-feature-post')[0].style.backgroundImage = '';
-    }
-    const postTitle = selector.getElementsByClassName('post-title')[0];
-    postTitle.innerHTML = post.title;
-    postTitle.href = '/posts/' + post.file;
-
-    /* if (post.excerpt) {
-         selector.getElementsByClassName('post-excerpt')[0].innerHTML = post.excerpt;
-     }*/
-    selector.style.display = 'block'
-}
-
-fetch('/posts/posts.json')
-    .then(response => response.json())
-    .then(data => {
-        posts = data;
-        const urlParams = new URLSearchParams(window.location.search);
-        const term = urlParams.get('search');
-        if (term) {
-            search(term)
-        } else if (location.pathname === '/') {
-            showPosts(posts);
-        }
-        else {
-            const index = posts.findIndex(x => x.file === location.pathname.replace('/posts/', ''));
-            if (index === -1) {
-                document.getElementById('previousPager').style.display = 'none';
-                document.getElementById('nextPager').style.display = 'none';
-            }
-            const pastPost = posts[index + 1];
-            const futurePost = posts[index - 1];
-
-            if (pastPost) updatePager(document.getElementById('previousPager'), pastPost);
-            else document.getElementById('previousPager').style.display = 'none';
-
-            if (futurePost) updatePager(document.getElementById('nextPager'), futurePost);
-            else document.getElementById('nextPager').style.display = 'none';
-        }
-    });
 
 function onLinkClick(e) {
     window.location.href = e.target.href;
--- make.js
+++ make.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
 const path = require('path');
+const minify = require('html-minifier').minify;
 
 const rootSite = 'https://eonasdan.com';
 
@@ -28,13 +29,26 @@ function removeDirectory(directory, removeSelf) {
         fs.rmdirSync(directory);
 }
 
+// since everyone has to have their own meta data *rolls eyes* the primary purpose here
+// is to quickly find similar tags and set them all at once
 function setMetaContent(rootElement, selector, content) {
     [...rootElement.getElementsByClassName(selector)].forEach(element => {
-        if (content) element.setAttribute("content", content);
-        else rootElement.getElementsByTagName('head')[0].removeChild(element);
+        if (content) {
+            element.setAttribute("content", content);
+            element.removeAttribute("class");
+        } else rootElement.getElementsByTagName('head')[0].removeChild(element);
     });
 }
 
+// doing this as a function so I don't have to null check values inline
+function setStructuredData(structure, property, value) {
+    if (!value) return;
+
+    structure[property] = value;
+}
+
+//read shell template
+const shellTemplate = fs.readFileSync(`./templates/shell-template.html`, 'utf8');
 
 //remove old stuff
 removeDirectory('./posts', false);
@@ -46,9 +60,15 @@ const posts = fs
 //create meta info
 let postsMeta = [];
 
+//prepare the static homepage text
+//todo at some point we'll have to deal with paging or infinite scrolls or something
+let homePageHtml = '';
+
 //create post files
 //read post template
-const postTemplate = fs.readFileSync(`./post-template.html`, 'utf8');
+const postTemplate = fs.readFileSync(`./templates/post-template.html`, 'utf8');
+
+//for each post partial, we create a full html page
 posts.forEach(file => {
     const newPageDocument = new JSDOM(postTemplate).window.document;
     const html = fs.readFileSync(`./post_partials/${file}`, 'utf8');
@@ -61,22 +81,41 @@ posts.forEach(file => {
     // strip html tags, line breaks and extra spaces
     const articleBody = stripHtml(article, ' ');
 
-    let baseMeta = {
+    let postMeta = {
         file: file,
         title: file.replace(path.extname(file), ''),
         body: articleBody,
-        "postDate": fs.statSync(`./post_partials/${file}`).mtime
+        postDate: fs.statSync(`./post_partials/${file}`).mtime
     };
 
     if (!hasMeta) {
-        postsMeta.push(baseMeta);
+        postsMeta.push(postMeta);
     } else {
-        const postMeta = JSON.parse(metaTag.textContent);
+
+        postMeta = {
+            ...postMeta,
+            ...JSON.parse(metaTag.textContent)
+        }
+
+        // create structured data
+        const structuredData = {
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
+            "author": {
+                "@type": "Person",
+                "name": "Jonathan Peterson",
+                "url": "https://eonasdan.com"
+            },
+        };
 
         newPageDocument.title = postMeta.title;
         if (postMeta.thumbnail) {
             newPageDocument.getElementById('post-thumbnail').innerHTML = `<img src="/img/${postMeta.thumbnail}" alt="" class="img-fluid"/>`;
-            setMetaContent(newPageDocument, 'metaImage', `${rootSite}/img/${postMeta.thumbnail}`);
+            const fullyQualifiedImage = `${rootSite}/img/${postMeta.thumbnail}`;
+            setMetaContent(newPageDocument, 'metaImage', fullyQualifiedImage);
+            setStructuredData(structuredData, 'image', [
+                fullyQualifiedImage
+                ]);
         } else {
             newPageDocument.getElementById('post-thumbnail').innerHTML = '';
             setMetaContent(newPageDocument, 'metaImage', '');
@@ -87,27 +126,67 @@ posts.forEach(file => {
         shareWidget.getElementsByClassName('linkedin')[0].href = `https://www.linkedin.com/sharing/share-offsite/?url=${location.href}`;*/
 
         setMetaContent(newPageDocument, 'metaTitle', postMeta.title);
+        setStructuredData(structuredData, 'headline', postMeta.title);
+
         setMetaContent(newPageDocument, 'metaDescription', postMeta.excerpt);
-        setMetaContent(newPageDocument, 'metaUrl', `${rootSite}/posts/${file}`);
-        setMetaContent(newPageDocument, 'metaPublishedTime', new Date(postMeta.postDate).toISOString());
+        const fullyQualifiedUrl = `${rootSite}/posts/${file}`;
+        setMetaContent(newPageDocument, 'metaUrl', fullyQualifiedUrl);
+        setStructuredData(structuredData, 'mainEntityOfPage', fullyQualifiedUrl);
+
+        const publishDate = new Date(postMeta.postDate).toISOString();
+        setMetaContent(newPageDocument, 'metaPublishedTime', publishDate);
+        setStructuredData(structuredData, 'datePublished', publishDate);
+
         //todo could use the fs.state to get the modified time, but I'd rather have control over that
         if (!postMeta.updateDate) postMeta.updateDate = postMeta.postDate;
-        setMetaContent(newPageDocument, 'metaModifiedTime', new Date(postMeta.updateDate).toISOString());
+        const updateDate = new Date(postMeta.updateDate).toISOString();
+        setMetaContent(newPageDocument, 'metaModifiedTime', updateDate);
+        setStructuredData(structuredData, 'dateModified', updateDate);
 
         setMetaContent(newPageDocument, 'metaTag', postMeta.tags);
+        setStructuredData(structuredData, 'keywords', postMeta.tags.split(', '));
 
-        postsMeta.push({
-            ...baseMeta,
-            ...postMeta
-        });
+        postsMeta.push(postMeta);
+
+        // push structured data to body
+        const structuredDataTag = newPageDocument.createElement("script");
+        structuredDataTag.type = "application/ld+json";
+        structuredDataTag.innerHTML = JSON.stringify(structuredData, null, 2);
+        newPageDocument.getElementsByTagName("body")[0].appendChild(structuredDataTag);
     }
+
+    const minifiedBody = minify(newPageDocument.getElementsByTagName('html')[0].innerHTML, {
+        //collapseWhitespace: true,
+        removeComments: true
+    });
 
     fs.writeFileSync(`./posts/${file}`,
         `<!DOCTYPE html>
-<html lang="en">${newPageDocument.getElementsByTagName('html')[0].innerHTML}
+<html lang="en">${minifiedBody}
 </html>`);
-});
 
+    //add to homepage html
+    homePageHtml += `<div class="single-post-area style-2">
+                        <div class="row align-items-center">
+                            <div class="col-12 col-md-6">
+                                <div class="post-thumbnail">
+                                    <img src="/img/${postMeta.thumbnail}" alt="" class="img-fluid"/>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="post-content mt-0">
+                                    <a href="/posts/${postMeta.file}" class="post-title mb-2">${postMeta.title}</a>
+                                    <div class="post-meta d-flex align-items-center mb-2">
+                                        <span class="post-author">By Jonathan Peterson</span>
+                                        <i class="far fa-circle" aria-hidden="true"></i>
+                                        <span class="post-date">${postMeta.postDate}</span>
+                                    </div>
+                                    <p class="mb-2">${postMeta.excerpt}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+});
 
 postsMeta = postsMeta
     .sort((a, b) => {
@@ -115,3 +194,18 @@ postsMeta = postsMeta
     });
 
 fs.writeFileSync('./posts/posts.json', JSON.stringify(postsMeta, null, 2));
+
+//set home page html
+(function() {
+    const indexDocument = new JSDOM(fs.readFileSync(`./templates/index.html`, 'utf8')).window.document;
+    indexDocument.getElementById('post-container').innerHTML = homePageHtml;
+    const minifiedBody = minify(indexDocument.getElementsByTagName('html')[0].innerHTML, {
+        collapseWhitespace: true,
+        removeComments: true
+    });
+
+    fs.writeFileSync(`./index.html`,
+        `<!DOCTYPE html>
+<html lang="en">${minifiedBody}
+</html>`);
+})();
